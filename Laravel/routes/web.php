fca7a862c4520389dd5cd1583f4352fab57b7c4a
sfreedomllq <sfreedomllq@gmail.com>
2019-07-25 00:56:34 +0000

    Add gravatar

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -20,3 +20,5 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('/help', 'StaticPagesController@help')->name('faq');
 
 Route::get('/signup', 'UsersController@create')->name('signup');
+
+Route::resource('users', 'UsersController');
