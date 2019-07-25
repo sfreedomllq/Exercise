09c0b5c54ec7419534fdac94602e7b5841bb861e
sfreedomllq <sfreedomllq@gmail.com>
2019-07-25 02:03:16 +0000

    Edit sign up page

--- Laravel/resources/views/users/create.blade.php
+++ Laravel/resources/views/users/create.blade.php
@@ -2,5 +2,36 @@
 @section('title', 'Sign up')
 
 @section('content')
-<h1>Sign up</h1>
+<div class="offset-md-2 col-md-8">
+  <div class="card">
+    <div class="card-header">
+      <h5>Sign Up</h5>
+    </div>
+    <div class="card-body">
+      <form action="{{ route('users.store') }}" method="POST">
+        <div class="form-group">
+          <label for="name" >名称：</label>
+          <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+        </div>
+
+        <div>
+          <label for="email" >邮箱：</label>
+          <input type="text" class="form-control" name="email" value="{{ old('email') }}">
+        </div>
+
+        <div>
+          <label for="password" >密码：</label>
+          <input type="password" class="form-control"  name="password" value="{{ old('password') }}">
+        </div>
+
+        <div>
+          <label for="password_confirmation" >确认密码：</label>
+          <input type="password" class="form-control" name="password_confirmation" value="{{ old('password_confirmation') }}">
+        </div>
+
+        <button type="submit" class="btn btn-primary">注册</button>
+      </form>
+    </div>
+  </div>
+</div>
 @stop
